96839f2fe6f95000ebf33120b9667fa36681c92b
Maru Schuindt <maruschuindt07@gmail.com>
2023-01-23 20:12:26 -0300

    Aclaraciones

--- Collection/src/Main.java
+++ Collection/src/Main.java
@@ -4,13 +4,16 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
+       //Asignacion de la lista
         List<Integer> ListaNumeros = new ArrayList<Integer>();
+
+       //Añadimos elementos a la lista 
         ListaNumeros.add(1);
         ListaNumeros.add(2);
         ListaNumeros.add(3);
 
-        //Implementacion del bucle for each
-        for (Integer valor: ListaNumeros){
+        //Implementacion del bucle for each para imprimir
+        for (Integer valor: ListaNumeros){ //Tipo de  dato que maneja, nombre para hacer referencia al for each donde guardara los numeros de la lista uno a uno.
             System.out.println(valor);
 
 
--- Collectionll/src/Main.java
+++ Collectionll/src/Main.java
@@ -7,24 +7,29 @@ public class Main {
 
      Scanner input = new Scanner(System.in);
         List<Integer> lisNum = new ArrayList<Integer>();
+
+        //Pedimos  al usuario la cantidad de numeros que quisiera en su lista
         System.out.println("Ingrese la cantidad de numeros a cargar: ");
         int N = input.nextInt();
 
+        //Guardamos los numeros con un bucle for, una a uno
         for (int i = 0; i< N; i++){
             System.out.println("Ingrese un numero nuevo");
             int numero = input.nextInt();
-            lisNum.add(numero);
+            lisNum.add(numero); //Añadimos a la lista uno a uno los numeros ingresados con ayuda del scanner y nuestra variable local
         }
 
+        //Forma tradicional del for, imprimiendo con un indice
      /*   for (int i = 0; i < lisNum.size(); i++) {
-            int elemento = lisNum.get(i);
-            System.out.println(elemento);
+            int elemento = lisNum.get(i);  //Get obtiene uno a uno los numeros de la lista
+            System.out.println(elemento); //elemento es donde guardamos los numeros uno a uno. Al estar  en un bucle, se rompe cuando no hayan mas numeros en la lista
         }
 
       */
 
+       //For each - Haciendo referencia el tipo de dato - nombre del for  each y que guardaremos de esa lista que estamos trabajando
         for (Integer elemento: lisNum) {
-            System.out.println(elemento);
+            System.out.println(elemento); //Imprime uno a uno, los numeros de la lista.
         }
 
 
